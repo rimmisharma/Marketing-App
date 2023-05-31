63261fbe46c87dd3097e29a48bbcd14175eed622
rimmisharma <rimmi1187.cse19@chitkara.edu.in>
2023-05-31 16:45:31 +0530

    final product ready

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import './App.css'; // Import the CSS file
-
-
+import React, { useState } from 'react';
+import './App.css';
 
 const AboutPage = () => {
- return (
-<div>
-<h2>About AlectoWatch</h2>
-<p>AlectoWatch is a state-of-the-art smart security system designed to provide comprehensive protection for your home. With advanced AI technology, it offers real-time monitoring, facial recognition, and intelligent access control. Experience peace of mind knowing that your home is safeguarded with the latest security measures.</p>
-</div>
- );
+  const [showEffect, setShowEffect] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowEffect(true);
+  };
+
+  return (
+    <div className="about-page">
+      <h2>About AlectoWatch</h2>
+      <p>
+        AlectoWatch is an innovative home security system that offers advanced features to protect your property and loved ones.
+        With cutting-edge technology and intelligent monitoring capabilities, AlectoWatch ensures your home's safety and provides you with peace of mind.
+        Whether it's real-time surveillance, smart access control, or personalized security recommendations, AlectoWatch has you covered.
+      </p>
+
+      <button onClick={handleButtonClick}>Click Me to get exclusive offer for you!</button>
+
+      {showEffect && (
+        <div className="effect">
+          <p>You will get exclusive 20% discount if you buy AlectoWatch using Axis bank credit card!</p>
+        </div>
+      )}
+    </div>
+  );
 };
-
-
 
 export default AboutPage;
--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,14 +1,69 @@
-import React from 'react';
-import './App.css'; // Import the CSS file
+import React, { useState } from 'react';
+import './App.css';
+import emailjs from 'emailjs-com';
 
 const ContactPage = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      from_name: values.name,
+      to_name: "AlectoWatch Admin!",
+      message: `Name: ${values.name}
+                Email: ${values.email}
+                Message: ${values.message}`,
+    };
+
+    emailjs
+      .send("service_2c2d5bx", "template_fwt6h5x", templateParams, "T-aolas4whLrWycQk")
+      .then((response) => {
+        console.log("Email sent successfully!");
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+
+    setValues({ name: '', email: '', message: '' });
+  };
+
   return (
     <div className="contact-container">
       <h2>Contact Us</h2>
-      <form className="contact-form">
-        <input type="text" name="name" placeholder="Your Name" required />
-        <input type="email" name="email" placeholder="Your Email" required />
-        <textarea name="message" placeholder="Your Message" required></textarea>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={values.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={values.email}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          value={values.message}
+          onChange={handleChange}
+          required
+        ></textarea>
         <button type="submit">Send Message</button>
       </form>
     </div>
